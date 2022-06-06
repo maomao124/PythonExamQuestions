48ee14a5c806beb8180f3b4ec0d009179a042554
mao <1296193245@qq.com>
2022-06-06 14:44:24 +0800

    PythonExamQuestions

---
+++ src/test6.py
@@ -0,0 +1,12 @@
+"""
+ * Project name(项目名称)：Python考试题
+ * Package(包名): 
+ * File(文件名): test6
+ * Author(作者）: mao
+ * Author QQ：1296193245
+ * GitHub：https://github.com/maomao124/
+ * Date(创建日期)： 2022/6/6 
+ * Time(创建时间)： 14:44
+ * Version(版本): 1.0
+ * Description(描述)： 
+ """
